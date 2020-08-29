e79866703b08a8373079f52635dfa1381cc26ddf
Mark Borysiak <borysiak.1@gmail.com>
2020-08-29 12:46:14 -0700

    fully functional app with project points included

--- Scripts/Simulation/RunSimulation.py
+++ Scripts/Simulation/RunSimulation.py
@@ -142,6 +142,10 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # Expected Points Functions
 #==================
 
+def csv_check(df, name):
+    return df.to_csv(f'c:/Users/mborysia/Desktop/FF_App/{name}.csv', index=False)
+
+
 def expected_pts_model(df):
     '''
     Linear Regression model that predicts points based on Position and Salary
@@ -237,33 +241,6 @@ def create_pt_dist(X_test, selected, lr_mean, lr_std):
     return team_dist
 
 
-def pts_over_expectation(df, lr_mean):
-    
-    # initialize list for storing values
-    pts_over_exp = []
-
-    # loop through each of the top 20 chosen players
-    for _, row in df.iterrows():
-
-        # find current players position and points mean
-        cur_pl = d[(d.index==row.Player) & (d.Position!='FLEX')].reset_index()
-        cur_pos = cur_pl.Position
-        cur_pts = cur_pl.iloc[:,2:999].mean(axis=1)
-
-        # convert the player information to X_test format to predict
-        row = pd.DataFrame(row).T
-        row['Position'] = cur_pos
-        row = row[['Position', 'AverageSalary']]
-        row.columns = ['Position', 'Salary']
-        X_test = create_X_test(row, test_template)
-
-        # determine extra pts above what was paid
-        extra_pts = cur_pts - lr_mean.predict(X_test)
-        pts_over_exp.append(extra_pts[0])
-    
-    return pts_over_exp
-
-
 #==========================
 # Submit Data Entry
 #==========================
@@ -340,8 +317,8 @@ drafted_player_table =  dash_table.DataTable(
 my_team_list = []
 for k, v in league_info['pos_require'].items():
     for i in range(v):
-        my_team_list.append([k, None, 0])
-my_team_df = pd.DataFrame(my_team_list, columns=['Position', 'Player', 'Salary'])
+        my_team_list.append([k, None, 0, 0])
+my_team_df = pd.DataFrame(my_team_list, columns=['Position', 'Player', 'Salary', 'Points Added'])
 
  # set up my team  drafted DataTable
 my_team_table =  dash_table.DataTable(
@@ -385,11 +362,11 @@ team_info_table =  dash_table.DataTable(
                         )
 
 
-# testing point distribution functions from above
+# # testing point distribution functions from above
 lr_mean, lr_std, test_template = expected_pts_model(d)
-remain_init, _ = salary_proportions(my_team_df, proport, league_info['salary_cap'])
-X_init = create_X_test(remain_init, test_template)
-team_dist = create_pt_dist(X_init, pd.DataFrame(), lr_mean, lr_std)
+# remain_init, selected = salary_proportions(my_team_df, proport, league_info['salary_cap'])
+# X_init = create_X_test(remain_init, test_template)
+# team_dist = create_pt_dist(X_init, selected, lr_mean, lr_std)
 
 #==========================
 # Plotting Functions
@@ -403,14 +380,15 @@ def create_bar(x_val, y_val, orient='h', color_str=main_color_rgba, text=None):
     return go.Bar(x=x_val, y=y_val, marker=marker_set, orientation=orient, text=text, showlegend=False)
 
 
-def create_fig_layout(fig1, fig2, fig3):
+def create_fig_layout(fig1, fig2):
     '''
     Function to combine bar charts into a single figure
     '''
-    fig = go.Figure(data=[fig1, fig2, fig3])
+    fig = go.Figure(data=[fig1, fig2])
     
     # Change the bar mode
-    fig.update_layout(barmode='group', autosize=True, height=800, margin=dict(l=0, r=25, b=0, t=15, pad=0),
+    fig.update_layout(barmode='group', autosize=True, height=800, 
+                      margin=dict(l=0, r=25, b=0, t=15, pad=0),
                       uniformtext_minsize=25, uniformtext_mode='hide')
     fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 
@@ -438,184 +416,217 @@ bar_gr = dcc.Graph(id='draft-results-graph')
 hist_gr = dcc.Graph(id='team-points-graph')
 
 
-# #============================
-# # Build out App Layout
-# #============================
+#============================
+# Build out App Layout
+#============================
 
-# app.layout = html.Div([
+app.layout = html.Div([
 
-#      html.Div([
-#          html.Div([
-#             html.H5("Enter Draft Pick Information"),
-#             drafted_player_table,
-#             ], className="four columns"),
+     html.Div([
+         html.Div([
+            html.H5("Enter Draft Pick Information"),
+            drafted_player_table,
+            ], className="four columns"),
 
-#             html.Div([
-#                 html.H5('My Team'),
-#                 my_team_table, html.Hr(),
-#                 submit_button, html.Hr(),
-#                 html.H5('Team Information'),
-#                 team_info_table,
-#                 html.Hr(),
-#                 hist_gr,  
-#             ], className='four columns'),
+            html.Div([
+                html.H5('My Team'),
+                my_team_table, html.Hr(),
+                submit_button, html.Hr(),
+                html.H5('Team Information'),
+                team_info_table,
+                html.Hr(),
+                hist_gr
+            ], className='four columns'),
 
-#             html.Div([
-#                 html.H5('Recommended Picks'),
-#                 bar_gr
-#             ], className="four columns")
+            html.Div([
+                html.H5('Recommended Picks'),
+                bar_gr
+            ], className="four columns")
        
-#        ], className="row2") ,        
+       ], className="row2") ,        
          
-# ])
+])
 
 
-# #============================
-# # Update Functions
-# #============================
+#============================
+# Update Functions
+#============================
 
-# def update_to_drop(df):
-#     '''
-#     INPUT
-#     -----
-#     Dataframe containing players + salaries to be dropped from selection
+def update_to_drop(df):
+    '''
+    INPUT: Dataframe containing players + salaries to be dropped from selection
 
-#     OUTPUT
-#     ------
-#     Dictionary containing dropped player + salaries for passing into simulation
-#     '''
-#     to_drop = {}
-#     to_drop['players'] = []
-#     to_drop['salaries'] = []
-#     for _, row in df.iterrows():
-#         if row.Salary > 0:
-#             to_drop['players'].append(row.Player)
-#             to_drop['salaries'].append(row.Salary)
+    OUTPUT: Dictionary containing dropped player + salaries for passing into simulation
+    '''
+    to_drop = {}
+    to_drop['players'] = []
+    to_drop['salaries'] = []
+    for _, row in df.iterrows():
+        if row.Salary > 0:
+            to_drop['players'].append(row.Player)
+            to_drop['salaries'].append(row.Salary)
 
-#     return to_drop
+    return to_drop
 
 
-# def update_to_add(df):
-#     '''
-#     INPUT
-#     -----
-#     Dataframe containing players + salaries to be added to my team
+def update_to_add(df):
+    '''
+    INPUT: Dataframe containing players + salaries to be added to my team
 
-#     OUTPUT
-#     ------
-#     Dictionary containing my team player + salaries for passing into simulation
-#     '''
-#     to_add = {}
-#     to_add['players'] = []
-#     to_add['salaries'] = []
-#     for _, row in df.iterrows():
-#         if row.Player is not None and row.Player!='' and row.Salary > 0:
-#             to_add['players'].append(row.Player)
-#             to_add['salaries'].append(row.Salary)
+    OUTPUT: Dictionary containing my team player + salaries for passing into simulation
+    '''
+    to_add = {}
+    to_add['players'] = []
+    to_add['salaries'] = []
+    for _, row in df.iterrows():
+        if row.Player is not None and row.Player!='' and row.Salary > 0:
+            to_add['players'].append(row.Player)
+            to_add['salaries'].append(row.Salary)
 
-#     return to_add
-
-# def team_fill(df, df2):
-#     '''
-#     INPUT
-#     -----
-#     df: blank team template to be filled in with chosen players
-#     df2: chosen players dataframe
-
-#     OUTPUT
-#     ------
-#     Dataframe filled in with selected player information
-#     '''
-#     # loop through chosen players dataframe
-#     for _, row in df2.iterrows():
-
-#         # pull out the current position and find min position index not filled (if any)
-#         cur_pos = row.Position
-#         min_idx = df.loc[(df.Position==cur_pos) & (df.Player.isnull())].index.min()
-
-#         # if position still needs to be filled, fill it
-#         if min_idx is not np.nan:
-#             df.loc[min_idx, ['Player', 'Salary']] = [row.Player, row.Salary]
-
-#         # if normal positions filled, fill in the FLEX if applicable
-#         elif cur_pos in ('RB', 'WR', 'TE'):
-#             cur_pos = 'FLEX'
-#             min_idx = df.loc[(df.Position==cur_pos) & (df.Player.isnull())].index.min()
-#             if min_idx is not np.nan:
-#                 df.loc[min_idx, ['Player', 'Salary']] = [row.Player, row.Salary]
-
-#             # otherwise, fill in the Bench
-#             else:
-#                 bench = pd.DataFrame(['Bench', row.Player, row.Salary]).T
-#                 bench.columns = ['Position', 'Player', 'Salary']
-#                 df = pd.concat([df, bench], axis=0)
-#     return df
+    return to_add
 
 
-
-# @app.callback([Output('draft-results-graph', 'figure'),
-#                Output('team-points-graph', 'figure'),
-#                Output('my-team-table', 'data'),
-#                Output('team-info-table', 'data')],
-#               [Input('submit-button-state', 'n_clicks')],
-#               [State('draft-results-table', 'data'),
-#                State('draft-results-table', 'columns')]
-# )
-# def update_output(n_clicks, drafted_data, drafted_columns):
-
-#     # get the list of drafted players
-#     drafted_df = pd.DataFrame(drafted_data, columns=[c['name'] for c in drafted_columns])
-#     drafted_df.Salary = drafted_df.Salary.astype('int')
-
-#     # update my team df to return
-#     my_team_update = my_team_df.copy()
-#     my_team_select = drafted_df[drafted_df['My Team']=='Yes'].reset_index(drop=True)
-#     drafted_df = drafted_df[drafted_df['My Team']!='Yes'].reset_index(drop=True)
-
-#     my_team_update = team_fill(my_team_update, my_team_select)
-
-#     # get lists of to_drop and to_add players
-#     to_drop = update_to_drop(drafted_df)
-#     to_add = update_to_add(my_team_update)
-
-#     # run the simulation
-#     _, inflation = sim.run_simulation(league_info, to_drop, to_add, iterations=iterations)
+def expected_pts(team_template, my_team_select, test_template):
+    '''
+    INPUT: Blank team template dataframe, dataframe of selected players, and the X_test
+           template the auto formats the one-hot-encoding for points prediction
     
-#     # get the results dataframe structured
-#     avg_sal = sim.show_most_selected(to_add, iterations, num_show=30)
-#     avg_sal = avg_sal.sort_values(by='Percent Drafted').reset_index()
-#     avg_sal = avg_sal.iloc[-20:]
-#     avg_sal.columns = ['Player', 'PercentDrafted', 'AverageSalary', 'ExpectedSalaryDiff']
-#     extra_pts = pts_over_expectation(avg_sal, lr_mean)
-#     avg_sal['ExtraPoints'] = extra_pts
+    OUTPUT: A dataframe with expected points added to your team based on average points 
+            scored by the player vs what would be expected based on position + salary regression
+    '''
 
-#     # Creating two subplots and merging into single figure
-#     # pick_text = [str(c) for c in avg_sal.PercentDrafted]
-#     # sal_text = [str(c) for c in avg_sal.AverageSalary]
-#     pick_bar = create_bar( list(avg_sal.PercentDrafted),list(avg_sal.Player), text=list(avg_sal.PercentDrafted))
-#     sal_bar = create_bar(list(avg_sal.AverageSalary), list(avg_sal.Player), 
-#                          color_str='rgba(250, 190, 88, 1)', text=avg_sal.AverageSalary)
-#     pts_bar = create_bar( list(avg_sal.ExtraPoints),list(avg_sal.Player), text=list(avg_sal.ExtraPoints))
-#     gr_fig = create_fig_layout(sal_bar, pick_bar, pts_bar)
+    #----------
+    # Create the initial baseline projection for your team
+    #----------
 
-#     # show drafted players
-#     remain_sal = league_info['salary_cap'] - np.sum(to_add['salaries'])
+    # copy the blank team template
+    my_team_template = team_template.copy()
+
+    # determine the remaining salary for the team across positions
+    remain_base, _ = salary_proportions(my_team_template, proport, league_info['initial_cap'])
     
-#     # histogram creation
-#     remain_pos, selected_df = salary_proportions(my_team_update, proport, remain_sal)
-#     X_test = create_X_test(remain_pos, test_template)
-#     hist_fig, pt_perc = create_pt_dist(X_test, selected_df, lr_mean, lr_std)
+    # create the X prediction for the base case and determine point distribution
+    X_base = create_X_test(remain_base, test_template)
+    team_dist_base = create_pt_dist(X_base, pd.DataFrame(), lr_mean, lr_std)
 
-#     # update team information table
-#     team_info_update = pd.DataFrame({'Mean Points': [pt_perc['50']], 
-#                                      'Inflation': [round(inflation,2)], 
-#                                      'Remaining Salary': [remain_sal]})
+    # loop through each player in selected players
+    exp_pts = []
+    for _, row in my_team_select.iterrows():
+
+        # get the team dataframe filled with only the current player
+        my_team_template = team_template.copy()
+        cur_player = team_fill(my_team_template, pd.DataFrame(row).T)
+        remain_pl, selected_pl = salary_proportions(cur_player, proport, league_info['initial_cap']-row.Salary)
+        
+        # predict points scored by the current player and all other blanks
+        X_pl = create_X_test(remain_pl, test_template)
+        team_dist_pl = create_pt_dist(X_pl, selected_pl, lr_mean, lr_std)
+        
+        # determine the number of points added by this player
+        pts_added = (team_dist_pl.mean() - team_dist_base.mean()).values[0]
+        exp_pts.append([row.Player, str(int(pts_added))])
+
+    return pd.DataFrame(exp_pts, columns=['Player', 'Points Added'])
+
+
+
+def team_fill(df, df2):
+    '''
+    INPUT: df: blank team template to be filled in with chosen players
+           df2: chosen players dataframe
+
+    OUTPUT: Dataframe filled in with selected player information
+    '''
+    # loop through chosen players dataframe
+    for _, row in df2.iterrows():
+
+        # pull out the current position and find min position index not filled (if any)
+        cur_pos = row.Position
+        min_idx = df.loc[(df.Position==cur_pos) & (df.Player.isnull())].index.min()
+
+        # if position still needs to be filled, fill it
+        if min_idx is not np.nan:
+            df.loc[min_idx, ['Player', 'Salary']] = [row.Player, row.Salary]
+
+        # if normal positions filled, fill in the FLEX if applicable
+        elif cur_pos in ('RB', 'WR', 'TE'):
+            cur_pos = 'FLEX'
+            min_idx = df.loc[(df.Position==cur_pos) & (df.Player.isnull())].index.min()
+            if min_idx is not np.nan:
+                df.loc[min_idx, ['Player', 'Salary']] = [row.Player, row.Salary]
+
+            # otherwise, fill in the Bench
+            else:
+                bench = pd.DataFrame(['Bench', row.Player, row.Salary]).T
+                bench.columns = ['Position', 'Player', 'Salary']
+                df = pd.concat([df, bench], axis=0)
+    return df
+
+
+
+@app.callback([Output('draft-results-graph', 'figure'),
+               Output('team-points-graph', 'figure'),
+               Output('my-team-table', 'data'),
+               Output('team-info-table', 'data')],
+              [Input('submit-button-state', 'n_clicks')],
+              [State('draft-results-table', 'data'),
+               State('draft-results-table', 'columns')]
+)
+def update_output(n_clicks, drafted_data, drafted_columns):
+
+    # get the list of drafted players
+    drafted_df = pd.DataFrame(drafted_data, columns=[c['name'] for c in drafted_columns])
+    drafted_df.Salary = drafted_df.Salary.astype('int')
+
+    # create a template of all team positions and players current selected for my team
+    my_team_template = my_team_df.copy()
+    my_team_select = drafted_df[drafted_df['My Team']=='Yes'].reset_index(drop=True)
+    my_team_update = team_fill(my_team_template.copy(), my_team_select)
+
+    # create a dataset of all other players that have been drafted
+    drafted_df = drafted_df[drafted_df['My Team']!='Yes'].reset_index(drop=True)
+
+    # get lists of to_drop and to_add players and remaining salary
+    to_drop = update_to_drop(drafted_df)
+    to_add = update_to_add(my_team_update)
+    remain_sal = league_info['salary_cap'] - np.sum(to_add['salaries'])
+
+    # run the simulation
+    _, inflation = sim.run_simulation(league_info, to_drop, to_add, iterations=iterations)
     
-#     # save out csv of status
-#     drafted_df.to_csv('c:/Users/mborysia/Desktop/Status_Save.csv', index=False)
+    # get the results dataframe structured
+    avg_sal = sim.show_most_selected(to_add, iterations, num_show=30)
+    avg_sal = avg_sal.sort_values(by='Percent Drafted').reset_index()
+    avg_sal = avg_sal.iloc[-20:]
+    avg_sal.columns = ['Player', 'PercentDrafted', 'AverageSalary', 'ExpectedSalaryDiff']
 
-#     return gr_fig, hist_fig, my_team_update.to_dict('records'), team_info_update.to_dict('records')
+    # Creating two subplots and merging into single figure
+    (pl, pc_dr, av_sl) = list(avg_sal.Player), list(avg_sal.PercentDrafted), avg_sal.AverageSalary
+    pick_bar = create_bar(pc_dr, pl, text=pc_dr)
+    sal_bar = create_bar(av_sl, pl, color_str='rgba(250, 190, 88, 1)', text=av_sl)
+    gr_fig = create_fig_layout(sal_bar, pick_bar)
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+    # histogram creation
+    remain_pos, selected_df = salary_proportions(my_team_update, proport, remain_sal)
+    X_test = create_X_test(remain_pos, test_template)
+ 
+    cur_team_dist = create_pt_dist(X_test, selected_df, lr_mean, lr_std)
+    hist_fig = create_hist(cur_team_dist)
+
+    if my_team_select.shape[0] > 0:
+        team_pts_added = expected_pts(my_team_template, my_team_select[['Position', 'Player', 'Salary']], test_template)
+        my_team_update = pd.merge(my_team_update.drop('Points Added', axis=1), team_pts_added, on='Player', how='left')
+        my_team_update['Points Added'] = my_team_update['Points Added'].fillna(0)
+
+    # update team information table
+    team_info_update = pd.DataFrame({'Mean Points': [int(cur_team_dist.mean())], 
+                                     'Inflation': [round(inflation,2)], 
+                                     'Remaining Salary': [remain_sal]})
+    
+    # save out csv of status
+    drafted_df.to_csv('c:/Users/mborysia/Desktop/Status_Save.csv', index=False)
+
+    return gr_fig, hist_fig, my_team_update.to_dict('records'), team_info_update.to_dict('records')
+if __name__ == '__main__':
+    app.run_server(debug=True)
 # %%
